babde9648b870ac4556011614cfcfdda6d5fee54
Devesh Kakad <deveshkakad2016@gmail.com>
2024-03-20 10:37:23 +0530

    redirection api handler

--- url_shortner.go
+++ url_shortner.go
@@ -48,6 +48,7 @@ func UrlShortnerHandler(writer http.ResponseWriter, request *http.Request) {
 		response := &ShortnedUrlResponse{
 			Shortner_Url: shortenedURL,
 		}
+		fmt.Println("Returning the existing shortned url for above domain")
 		ndata, _ := json.Marshal(&response)
 		ndata = append(ndata, '\n')
 		writer.Write([]byte(ndata))
@@ -58,11 +59,25 @@ func UrlShortnerHandler(writer http.ResponseWriter, request *http.Request) {
 	response := &ShortnedUrlResponse{
 		Shortner_Url: shortenedURL,
 	}
+	fmt.Println("Adding shortned url in the map and returning the result")
 	ndata, _ := json.Marshal(&response)
 	ndata = append(ndata, '\n')
 	writer.Write([]byte(ndata))
 }
+func RedirectHandler(writer http.ResponseWriter, request *http.Request) {
+	shortenedURL := request.URL.Path[1:]
+	fmt.Printf("shortenedurl received:%v\n", shortenedURL)
+	originalURL, ok := urlMapping[shortenedURL]
+	fmt.Printf("urlMapping:%v", urlMapping)
+	fmt.Printf("shortenedurl received:%v\n", originalURL)
+	if !ok {
+		http.NotFound(writer, request)
+		return
+	}
+	http.Redirect(writer, request, originalURL, http.StatusFound)
+}
 func main() {
 	http.HandleFunc("/shorten", UrlShortnerHandler)
+	http.HandleFunc("/", RedirectHandler)
 	http.ListenAndServe(":8080", nil)
 }
